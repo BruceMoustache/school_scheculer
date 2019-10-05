d7cda4279e86e62dea7b100ae91f2e33e4afad04
BruceMustache <viniciusxnicolas007@gmail.com>
2019-10-04 23:23:17 -0300

    updating config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-available_days_interval = 1, 5
+available_days_interval = 'sunday', 'friday'
 counter_classes_per_day = 4
 map_classes_per_week = {
-	'mathematics': 4,
-	'portuguese': 2,
-	'english': 2,
+	'mathematics': 6,
+	'portuguese': 4,
+	'english': 4,
 	'programming': 6,
 }
