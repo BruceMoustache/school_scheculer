c41a1e6318f9e139e32b82c6430e3a6bdad0dfbd
BruceMustache <viniciusxnicolas007@gmail.com>
2019-10-04 22:50:50 -0300

    working fine

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-classes_per_day = 4
-classes_per_week = {
+available_days_interval = 1, 5
+counter_classes_per_day = 4
+map_classes_per_week = {
 	'mathematics': 4,
 	'portuguese': 2,
 	'english': 2,
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-from config import classes_per_week, classes_per_day
+from config import map_classes_per_week, counter_classes_per_day, available_days_interval
 
 
 standard_day_reference = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'sturday']
@@ -7,9 +7,24 @@ standard_day_reference = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday'
 def get_day_name(index, reference = standard_day_reference):
 	return reference[index]
 
+def show_schedule(schedule):
+	for day_index, day_list in enumerate(schedule):
+		print(get_day_name(day_index))
+		for subject in day_list:
+			print('\t', subject)
+
 
 single_schedule = []
 for counter in range(len(standard_day_reference)):
-	single_schedule.append([None] * classes_per_day)
-print(single_schedule)
+	single_schedule.append([None] * counter_classes_per_day)
+
+for subject in map_classes_per_week:
+	remaining_sheduling = map_classes_per_week[subject]
+	while remaining_sheduling > 0:
+		subject_day_index = random.randint(*available_days_interval)
+		subject_time_index = random.randint(0, counter_classes_per_day - 1)
+		single_schedule[subject_day_index][subject_time_index] = subject
+		remaining_sheduling -= 1
+
+show_schedule(single_schedule)
 
